7cb848aebfbcfbfc70472d21ce1e83e91eae2d66
Baynrich <bendikasolevaag@gmail.com>
2022-02-01 19:36:24 +0100

    lint

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <section className="z-10 relative pt-16 sm:pt-[20vh] max-w-wrapper mx-auto px-4 flex flex-col sm:flex-row sm:justify-between">
           <div className="text-white sm:w-[40%]">
               <h1>Erik Hystad</h1>
-              <p className="mt-4 text-lead-medium">Master's student and AI enthusiast at the University of Bergen, Norway.</p>
+              <p className="mt-4 text-lead-medium">Master&apos;s student and AI enthusiast at the University of Bergen, Norway.</p>
           </div>
           <figure className='mt-16 sm:-mt-8'>
             <Image src="/portrett.jpg" width={350} height={450} alt="Portrett av Erik Hystad" />

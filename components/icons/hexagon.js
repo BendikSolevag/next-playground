779f96d6a42e4a13ce831d3ddf984ba81ee6d363
Baynrich <bendikasolevaag@gmail.com>
2022-02-01 18:30:05 +0100

    Fix header overlapping issue

--- components/icons/hexagon.js
+++ components/icons/hexagon.js
@@ -1,7 +1,7 @@
 export default function Hexagon() {
     return(
-        <svg className="-z-10" width="auto" height="auto" viewBox="0 0 84 96" fill="white" xmlns="http://www.w3.org/2000/svg">
-            <path d="M42 0L83.5692 24V72L42 96L0.430782 72V24L42 0Z" fill="#FFFFFF"/>
+        <svg className="-z-10" width="auto" height="auto" viewBox="0 0 96 84" fill="white" xmlns="http://www.w3.org/2000/svg">
+            <path d="M24 0.430782L72 0.430785L96 42L72 83.5692L24 83.5692L6.88655e-06 42L24 0.430782Z" fill="#FFFFFF"/>
         </svg>
     )
 }
